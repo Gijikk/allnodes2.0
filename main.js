bb4024507573f227879caca346f13373c6786b3d
Yan Ryzhkov <passionary003@gmail.com>
2022-10-26 00:24:45 +0300

    fix scroll and some bags

--- main.js
+++ main.js
@@ -141,25 +141,25 @@
         };
         SliceSlider.init();
     })(jQuery);
-    const cursor = document.querySelector(".view");
-    var timeout;
-
-    //follow cursor on mousemove
-    document.addEventListener("mousemove", (e) => {
-        let x = e.pageX;
-        let y = e.pageY;
-
-        cursor.style.top = y + "px";
-        cursor.style.left = x + "px";
-        cursor.style.display = "block";
-
-        //cursor effects when mouse stopped
-        function mouseStopped() {
-            cursor.style.display = "none";
-        }
-        clearTimeout(timeout);
-        timeout = setTimeout(mouseStopped, 1000);
-    });
+    // const cursor = document.querySelector(".view");
+    // var timeout;
+    //
+    // //follow cursor on mousemove
+    // document.addEventListener("mousemove", (e) => {
+    //     let x = e.pageX;
+    //     let y = e.pageY;
+    //
+    //     cursor.style.top = y + "px";
+    //     cursor.style.left = x + "px";
+    //     cursor.style.display = "block";
+    //
+    //     //cursor effects when mouse stopped
+    //     function mouseStopped() {
+    //         cursor.style.display = "none";
+    //     }
+    //     clearTimeout(timeout);
+    //     timeout = setTimeout(mouseStopped, 1000);
+    // });
 
 
 const tl = gsap.timeline({
@@ -287,3 +287,81 @@ tl.to(".footerContent", {
         });
 
     }).call(this);
+
+
+    /*
+    pointer.js was created by OwL for use on websites,
+     and can be found at https://seattleowl.com/pointer.
+*/
+
+    const pointer = document.querySelector(".view")
+    pointer.id = "pointer-dot"
+    const ring = document.createElement("div")
+    ring.id = "pointer-ring"
+    document.body.insertBefore(pointer, document.body.children[0])
+    document.body.insertBefore(ring, document.body.children[0])
+
+    let mouseX = -100
+    let mouseY = -100
+    let ringX = -100
+    let ringY = -100
+    let isHover = false
+    let mouseDown = false
+    const init_pointer = (options) => {
+
+        window.onmousemove = (mouse) => {
+            mouseX = mouse.clientX
+            mouseY = mouse.clientY
+        }
+
+        window.onmousedown = (mouse) => {
+            mouseDown = true
+        }
+
+        window.onmouseup = (mouse) => {
+            mouseDown = false
+        }
+
+        const trace = (a, b, n) => {
+            return (1 - n) * a + n * b;
+        }
+        window["trace"] = trace
+
+        const getOption = (option) => {
+            let defaultObj = {
+                pointerColor: "#07658c",
+                ringSize: 15,
+                ringClickSize: (options["ringSize"] || 15) - 5,
+            }
+            if (options[option] == undefined) {
+                return defaultObj[option]
+            } else {
+                return options[option]
+            }
+        }
+
+        const render = () => {
+            ringX = trace(ringX, mouseX, 0.2)
+            ringY = trace(ringY, mouseY, 0.2)
+
+            if (document.querySelector(".p-action-click:hover")) {
+                pointer.style.borderColor = getOption("pointerColor")
+                isHover = true
+            } else {
+                pointer.style.borderColor = "white"
+                isHover = false
+            }
+            ring.style.borderColor = getOption("pointerColor")
+            if (mouseDown) {
+                ring.style.padding = getOption("ringClickSize") + "px"
+            } else {
+                ring.style.padding = getOption("ringSize") + "px"
+            }
+
+            pointer.style.transform = `translate(${mouseX}px, ${mouseY}px)`
+            ring.style.transform = `translate(${ringX - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px, ${ringY - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px)`
+
+            requestAnimationFrame(render)
+        }
+        requestAnimationFrame(render)
+    }
